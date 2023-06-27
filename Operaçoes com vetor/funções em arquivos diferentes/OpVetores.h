1e24366b5c77e2e5f1c1c0eadfc0728b944d6811
Isabel de Freitas <isabeldefreitasbarboza@gmail.com>
2023-06-26 23:28:13 -0300

    protótipo das funções

---
+++ Operaçoes com vetor/funções em arquivos diferentes/OpVetores.h
@@ -0,0 +1,11 @@
+#ifndef OpVetores_H
+#define OpVetores_H
+
+void mostrarConteudo(int* vetNum, int numEl);
+int somarNumero(int* vetNum, int numEl);
+int encontrarMaior(int* vetNum, int numEl);
+int encontrarPosicaoMaior(int* vetNum, int numEl);
+int encontrarMenor(int* vetNum, int numEl);
+int encontrarPosicaoMenor(int* vetNum, int numEl);
+
+#endif
